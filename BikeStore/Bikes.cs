68041eed80f0b4ce0d54bd8af749d38b5bf0eda2
BTCAM <104627587+BTCAM@users.noreply.github.com>
2025-04-24 19:26:49 +0100

    Added the Bikes Class to be used to create and get details of bikes in the program

---
+++ BikeStore/Bikes.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BikeStore
+{
+    public class Bikes
+    {
+        public string BikeID {  get; set; }
+        public string BikeType { get; set; }
+
+        public string Address { get; set; }
+
+        //Constructor to create any Bike Objects
+        public Bikes(string bikeID, string bikeType, string address)
+        {
+            BikeID = bikeID;
+            BikeType = bikeType;
+            Address = address;
+        }
+
+        //Method to get details of Bike
+        public string GetDetails()
+        {
+            return BikeType  + " - " + Address + " ID: " + BikeID;
+        }
+
+    }
+}

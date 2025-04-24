b32afa90706a8d3c1676a152f6a004c54995894a
BTCAM <104627587+BTCAM@users.noreply.github.com>
2025-04-24 19:17:55 +0100

    Add project files.

---
+++ BikeStore/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace BikeStore
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+
+
+        }
+    }
+}
